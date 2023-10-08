1a00b3548f8c4d39fd4b4e327602106a64be908b
HO NGOC Y <ngocyho1712@gmail.com>
2023-10-08 18:34:21 +0700

    create drawer navitation+ fragment+login layout

--- app/src/main/java/com/example/readingbook/MainActivity.java
+++ app/src/main/java/com/example/readingbook/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.readingbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.example.readingbook.fragment.Book_Fragment;
+import com.example.readingbook.fragment.Customer_Fragment;
+import com.example.readingbook.fragment.Kind_of_book_Fragment;
+import com.example.readingbook.fragment.PhieuMuon_Fragment;
+import com.example.readingbook.fragment.Staff_Fragment;
+import com.example.readingbook.fragment.Statiscal_Fragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.setDrawerIndicatorEnabled(true);
         drawerToggle.syncState();
         drawerLayout.addDrawerListener(drawerToggle);
+        // gọi các id của drawer_navigation
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                if(item.getItemId()==R.id.staff_manager){
+                    toolbar.setTitle("Staff manager");
+                    fragment = new Staff_Fragment();
+                } else if (item.getItemId()==R.id.customer_manager) {
+                    toolbar.setTitle("Customer manager");
+                    fragment = new Customer_Fragment();
+                }else if (item.getItemId()==R.id.kind_book_manager) {
+                    toolbar.setTitle("Kind of book manager");
+                    fragment = new Kind_of_book_Fragment();
+                }else if (item.getItemId()==R.id.book_manager) {
+                    toolbar.setTitle("Book manager");
+                    fragment = new Book_Fragment();
+                }else if (item.getItemId()==R.id.phieu_muon_manager) {
+                    toolbar.setTitle("Loan slip manager");
+                    fragment = new PhieuMuon_Fragment();
+                }else if (item.getItemId()==R.id.statistical_manager) {
+                    toolbar.setTitle("Statistical manager");
+                    fragment = new Statiscal_Fragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment).commit();
+                return true;
+            }
+        });
     }
 
     private void addControls() {
